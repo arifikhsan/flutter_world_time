47e0a2284297b1ff36bb6b200c7f2b382819e340
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2019-12-11 21:11:21 +0700

    ,

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -21,8 +21,11 @@ class _HomeState extends State<Home> {
       backgroundColor: bgColor,
       body: Container(
         decoration: BoxDecoration(
-            image: DecorationImage(
-                image: AssetImage('assets/$bgImage'), fit: BoxFit.cover)),
+          image: DecorationImage(
+            image: AssetImage('assets/$bgImage'),
+            fit: BoxFit.cover,
+          ),
+        ),
         child: Padding(
           padding: const EdgeInsets.fromLTRB(0, 120, 0, 0),
           child: SafeArea(
@@ -30,7 +33,8 @@ class _HomeState extends State<Home> {
               children: <Widget>[
                 FlatButton.icon(
                   onPressed: () async {
-                    dynamic result = await Navigator.pushNamed(context, '/location');
+                    dynamic result =
+                        await Navigator.pushNamed(context, '/location');
                     setState(() {
                       data = {
                         'time': result['time'],
